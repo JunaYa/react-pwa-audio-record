e8b9bcf43c749679329e9ac73fa16eee0fbf117e
JunaYa <ayac3j@gmail.com>
2023-08-29 14:38:44 +0800

    chore: use audio recorder

--- src/app/[lang]/publish/page.tsx
+++ src/app/[lang]/publish/page.tsx
@@ -1,14 +1,12 @@
 'use client'
-import AudioRecorder from "~/ui/Recorder";
+import AudioRecorder from "~/ui/AudioRecorder";
 
 export default function Home() {
-
-  const onFinish = () => {}
 
   return (
     <main className="lex h-full flex-col items-center justify-between px-24">
       <div className='w-48 rounded bg-white text-center font-bold shadow'>Title</div>
-      <AudioRecorder onFinish={onFinish}/>
+      <AudioRecorder />
     </main>
   )
 }
